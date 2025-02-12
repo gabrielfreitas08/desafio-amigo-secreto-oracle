74c1bd4f2dc76a9ba625233a858e28741f20dfbf
Gabriel Dias Freitas <880gabrielfreitas.com>
2025-02-12 16:55:22 -0300

    validação se o campo contém valor e limpando o campo após a inserção do nome

--- app.js
+++ app.js
@@ -6,8 +6,17 @@ var exibirNome = document.getElementById("listaAmigos");
 function adicionarAmigo() {
     
     let pegarNome = document.getElementById("amigo").value; //pega o valor digitado no campo
-    adicionarNome.push(pegarNome); //guarda os nome no array
-    exibirNome.innerHTML = adicionarNome;
-    //console.log(adicionarNome);
+    if (pegarNome == "") {
+        alert("Por favor, insira um nome");
+    } else {
+        adicionarNome.push(pegarNome); //guarda os nome no array
+        exibirNome.innerHTML = adicionarNome;
+    }
+    limparCampo();
+}
+
+function limparCampo() {
     
+    pegarNome = document.getElementById("amigo");
+    pegarNome.value = "";
 }
